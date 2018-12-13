dd4a15f5d4060deae303bc1c9fee94895e03f0d7
rhindle <knowlton@gmail.com>
2018-12-13 00:04:27 -0500

    Fixed MsiCameraMMC code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MsiCameraMMC_lk.java
@@ -0,0 +1,164 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+import java.util.List;
+
+import static java.lang.Thread.sleep;
+
+/**
+ * This 2018-2019 OpMode illustrates the basics of using the TensorFlow Object Detection API to
+ * determine the position of the gold and silver minerals.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
+ *
+ * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
+ * is explained below.
+ */
+
+//public abstract class MsiCameraMMC extends LinearOpMode {
+public class MsiCameraMMC_lk {             //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    private static final String TFOD_MODEL_ASSET = "RoverRuckus.tflite";
+    private static final String LABEL_GOLD_MINERAL = "Gold Mineral";
+    private static final String LABEL_SILVER_MINERAL = "Silver Mineral";
+
+    private HardwareMap hardwareMap;    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    private Telemetry telemetry;        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+    /*
+     * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
+     * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
+     * A Vuforia 'Development' license key, can be obtained free of charge from the Vuforia developer
+     * web site at https://developer.vuforia.com/license-manager.
+     *
+     * Vuforia license keys are always 380 characters long, and look as if they contain mostly
+     * random data. As an example, here is a example of a fragment of a valid key:
+     *      ... yIgIzTqZ4mWjk9wd3cZO9T1axEqzuhxoGlfOOI2dRzKS4T0hQ8kT ...
+     * Once you've obtained a license key, copy the string from the Vuforia web site
+     * and paste it in to your code on the next line, between the double quotes.
+     */
+    private static final String VUFORIA_KEY = "Af6s/L//////AAABmRhSfCy6B0HFrA+rWBwSOjOLybbnvggOtNv1qQcKzWQeehDdLo7Rp/PjEsmQEkAVVG65fkwcLBXyAMdGzOGCTIdZ/s0Qo0Y50Iz2mYmayUvmTu4jhJSiRpcr6dLCg9guNe7awzfAxX2Bwb9A3V9vW0T+EUPsEWci5tUeosyli1keHgxcT3IaQvs0217TPzfty3lQKKBitm1jhzf7dh0vPBMdZRT91M6+cQQzBNA9NXxxCMjl9ZwXA6mTzLO43fO7Gg2HOanGfhAJytyZD+DeuGtzJWQxjuRydRIsNxpsbUAS5HmVaA9zWyaE3UU2AuEOzOobMEcm+jZs8Ha/tAKwwAZJcn9fEpHChSBtPmJQ2h1E\n";
+
+    /**
+     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
+     * localization engine.
+     */
+    private VuforiaLocalizer vuforia;
+
+    /**
+     * {@link #tfod} is the variable we will use to store our instance of the Tensor Flow Object
+     * Detection engine.
+     */
+    private TFObjectDetector tfod;
+
+//    public void FindGoald() {
+    public int FindGoald(OpMode opmode) {          //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        this.hardwareMap = opmode.hardwareMap;     //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        this.telemetry = opmode.telemetry;         //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        ElapsedTime timer = new ElapsedTime();     //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        int findstatus = 0;                        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
+        // first.
+        initVuforia();
+
+        if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
+            initTfod();
+        } else {
+            telemetry.addData("Sorry!", "This device is not compatible with TFOD");    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        }
+
+        if (tfod != null) {
+            tfod.activate();
+        }
+
+        timer.reset();    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        while ((findstatus == 0) && (timer.time() < 5)) {        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            if (tfod != null) {
+                // getUpdatedRecognitions() will return null if no new information is available since
+                // the last time that call was made.
+                List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                if (updatedRecognitions != null) {
+                    telemetry.addData("# Object Detected", updatedRecognitions.size());   //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                    if (updatedRecognitions.size() == 3) {
+                        int goldMineralX = -1;
+                        int silverMineral1X = -1;
+                        int silverMineral2X = -1;
+                        for (Recognition recognition : updatedRecognitions) {
+                            if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
+                                goldMineralX = (int) recognition.getLeft();
+                            } else if (silverMineral1X == -1) {
+                                silverMineral1X = (int) recognition.getLeft();
+                            } else {
+                                silverMineral2X = (int) recognition.getLeft();
+                            }
+                        }
+                        if (goldMineralX != -1 && silverMineral1X != -1 && silverMineral2X != -1) {
+                            if (goldMineralX < silverMineral1X && goldMineralX < silverMineral2X) {
+                                telemetry.addData("Gold Mineral Position", "Left");   //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                                findstatus = 1;
+                            } else if (goldMineralX > silverMineral1X && goldMineralX > silverMineral2X) {
+                                telemetry.addData("Gold Mineral Position", "Right");   //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                                findstatus = 2;
+                            } else {
+                                telemetry.addData("Gold Mineral Position", "Center");   //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                                findstatus = 3;
+                            }
+                        }
+                    }
+                    telemetry.update();     //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                }
+            }
+        }
+
+        if (tfod != null) {
+            tfod.shutdown();
+        }
+
+        // For debugging, so we can see the last message on the telemetry                    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        try { sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }   //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        return findstatus;                                                                   //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+
+    /**
+     * Initialize the Vuforia localization engine.
+     */
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = CameraDirection.BACK;
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Loading trackables is not necessary for the Tensor Flow Object Detection engine.
+    }
+
+    /**
+     * Initialize the Tensor Flow Object Detection engine.
+     */
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_GOLD_MINERAL, LABEL_SILVER_MINERAL);
+    }
+}
